84c396b2b3c6da22f386cb7e784c0254b3db7e4f
Tiago Machado <tiago.o.machado@gmail.com>
2018-12-24 00:11:24 -0200

    Carrinho Calculo de Frete

--- site.php
+++ site.php
@@ -58,7 +58,8 @@ $app->get("/cart", function() {
 	$page = new Page();
 	$page->setTpl("cart", [
 		'cart'		=>	$cart->getValues(),
-		'products'	=>	$cart->getProducts()
+		'products'	=>	$cart->getProducts(),
+		'error'		=>	Cart::getMsgError()
 	]);
 });
 
@@ -94,6 +95,15 @@ $app->get("/cart/:idproduct/remove", function($idproduct) {
 
 	$cart = Cart::getFromSession();
 	$cart->removeProduct($product, true);
+
+	header("Location: /cart");
+	exit();
+});
+
+// Calcular frete dos produtos do carrinho
+$app->post("/cart/freight", function() {
+	$cart = Cart::getFromSession();
+	$cart->setFreight($_POST['zipcode']);
 
 	header("Location: /cart");
 	exit();
