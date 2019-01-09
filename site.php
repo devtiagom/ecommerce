0ed4ea687628052fb16c520f45c138532696fc04
Tiago Machado <tiago.o.machado@gmail.com>
2019-01-08 23:13:27 -0200

    Site Cadastro

--- site.php
+++ site.php
@@ -130,7 +130,9 @@ $app->get("/checkout", function() {
 $app->get("/login", function() {
 	$page = new Page();
 	$page->setTpl("login", [
-		'error'	=>	User::getError()
+		'error'			=>	User::getError(),
+		'errorRegister'	=>	User::getErrorRegister(),
+		'registerValues'=>	$_SESSION['registerValues'] ?? ['name' => '', 'email' => '', 'phone' => '']
 	]);
 });
 
@@ -151,5 +153,53 @@ $app->get("/logout", function() {
 	User::logout();
 
 	header("Location: /login");
+	exit();
+});
+
+// Cadastro de usuário cliente
+$app->post("/register", function() {
+	$_SESSION['registerValues'] = $_POST;
+
+	if (!isset($_POST['name']) || $_POST['name'] == '') {
+		User::setErrorRegister("Preencha o seu nome.");
+
+		header("Location: /login");
+		exit();
+	}
+
+	if (!isset($_POST['email']) || $_POST['email'] == '') {
+		User::setErrorRegister("Preencha o seu e-mail.");
+
+		header("Location: /login");
+		exit();
+	}
+
+	if (!isset($_POST['password']) || $_POST['password'] == '') {
+		User::setErrorRegister("Preencha a sua senha.");
+
+		header("Location: /login");
+		exit();
+	}
+
+	if (User::checkLoginExist($_POST['email'])) {
+		User::setErrorRegister("Este e-mail já existe em nosso cadastro.");
+
+		header("Location: /login");
+		exit();
+	}
+
+	$user = new User();
+	$user->setData([
+		'inadmin'		=>	0,
+		'deslogin'		=>	$_POST['email'],
+		'desperson'		=>	$_POST['name'],
+		'desemail'		=>	$_POST['email'],
+		'despassword'	=>	$_POST['password'],
+		'nrphone'		=>	$_POST['phone']
+	]);
+	$user->save();
+	User::login($_POST['email'], $_POST['password']);
+
+	header("Location: /checkout");
 	exit();
 });
--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -320,8 +320,25 @@ class User extends Model {
 
     /**********************************************************************************************************/
 
-    public static function setErrorRegister() {
+    public static function setErrorRegister($msg) {
         $_SESSION[User::ERROR_REGISTER] = $msg;
+    }
+
+    /**********************************************************************************************************/
+
+    public static function getErrorRegister() {
+        $msg = (isset($_SESSION[User::ERROR_REGISTER]) && $_SESSION[User::ERROR_REGISTER]) ?
+            $_SESSION[User::ERROR_REGISTER] : '';
+
+        User::clearErrorRegister();
+
+        return $msg;
+    }
+
+    /**********************************************************************************************************/
+
+    public static function clearErrorRegister() {
+        $_SESSION[User::ERROR_REGISTER] = NULL;
     }
 
     /**********************************************************************************************************/
